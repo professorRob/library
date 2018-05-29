f5f713e10b64af6ed92d2323f7e15a4c9796297e
professorrob78@yahoo.com <professorrob78@yahoo.com>
2018-05-29 12:04:07 -0400

    goheadon

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YourLivingHope.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DailyDevotion()
+        {
+
+            return View();
+        }
+
+        public ActionResult Testimony()
+        {
+            return View();
+        }
+
+        public ActionResult Worship()
+        {
+            return View();
+        }
+
+        public ActionResult Prayer()
+        {
+            return View();
+        }
+
+        public ActionResult TheWord()
+        {
+            return View();
+        }
+    }
+}
